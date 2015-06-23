66c336fea8b7efdf096397c4a3137da4ab2fb179
Kevin Downey <redchin@gmail.com>
2015-06-23 10:11:05 -0700

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.manigfeald/optimizer "0.1.0"
+(defproject com.manigfeald/optimizer "0.2.0-SNAPSHOT"
   :description "Optimize a parameter"
   :url "http://github.com/hiredman/optimizer"
   :license {:name "Eclipse Public License"
