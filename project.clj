d0e056f307de830309c6e21fdc78f2dbd16e21d2
Kevin Downey <redchin@gmail.com>
2015-06-22 23:31:46 -0700

    urls and stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.manigfeald/optimizer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Optimize a parameter"
+  :url "http://github.com/hiredman/optimizer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
